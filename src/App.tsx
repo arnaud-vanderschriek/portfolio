44181d6aa6eeace11d245252aec40458cd19058f
D <arnaud.vanderschrieck@gmail.com>
2022-10-14 12:20:50 +0200

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
 import LoadingPage from './components/loadingPage/LoadinPage';
 import Front from './components/front/Front';
 import Back from './components/back/Back';
 import "./assets/sass/App.scss"
 
+
 function App() {
    return (
-      <Router>
+    <Router>
+      <HashRouter basename='/portfolio'>
         <Routes>
           <Route path="/" element={ <LoadingPage />} />
           <Route path='/front' element={ <Front />} />
           <Route path='/back' element={ <Back />} />
         </Routes>
-     </Router>
-  );
+      </HashRouter>
+    </Router> 
+  )
 }
 
 export default App;
