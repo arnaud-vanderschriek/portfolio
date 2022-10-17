f147cdb42f1b1088b6c30f9bd2b0fe56b42a5c9f
D <arnaud.vanderschrieck@gmail.com>
2022-10-17 20:24:01 +0200

    modify BrowserRouter for github

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LoadingPage from './components/loadingPage/LoadinPage';
 import Front from './components/front/Front';
 import Back from './components/back/Back';
 import "./assets/sass/App.scss"
 
 
-function App() {
+export const App = () => {
    return (
-    <Router>
-        <Routes>
-          <Route path="/portfolio " element={ <LoadingPage />} />
-          <Route path='/portfolio/front' element={ <Front />} />
-          <Route path='/portfolio/back' element={ <Back />} />
-        </Routes>
-    </Router> 
+    <Routes>
+      <Route path="/" element={<LoadingPage />} />
+      <Route path='/portfolio/front' element={<Front />} />
+      <Route path='/portfolio/back' element={<Back/>} />
+    </Routes> 
   )
 }
 
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import {App} from './App';
 import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 
 const container = document.getElementById('root')!;
